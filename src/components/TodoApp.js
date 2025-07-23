b3a5bd6a05c99946323049141fbd42f8a38e3243
p1zzakat <rbanine@gmail.com>
2025-07-23 18:22:46 +0100

    create components and initial page setup

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+export class TodoApp {
+
+    constructor(root) {
+
+        this.root = root;
+        this.root.innerHTML = 
+        `
+            <h1>sup</h1>
+        `;
+
+    }
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('template');
+import { TodoApp } from "./components/TodoApp";
+
+const main = document.querySelector(".main");
+
+const todoApp = new TodoApp(main);
