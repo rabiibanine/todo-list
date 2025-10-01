0f5401fb6ea8c75f863d03fa7a44ad5b00e0a08c
p1zzakat <rbanine@gmail.com>
2025-10-01 17:09:38 +0100

    Add more task features

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,37 +1,45 @@
 import Remove from "../public/remove.svg";
+import Edit from "../public/edit.svg";
 
 export class Task {
-
     constructor(taskData) {
-
-        this.title = taskData.title;
-        this.description = taskData.description;
+        this.title = taskData.title ? taskData.title : "Unnamed";
+        this.description = taskData.description ? taskData.description : "No description";
         this.dueDate = taskData.dueDate;
         this.priority = taskData.priority;
         this.id = crypto.randomUUID();
-
     }
 
     constructElement() {
-
-        const taskElement = document.createElement('div');
-        taskElement.classList.add('main__content-task');
+        const taskElement = document.createElement("div");
+        taskElement.classList.add("main__content-task");
         taskElement.dataset.id = this.id;
-        const taskTitleElement = document.createElement('h2');
-        taskTitleElement.classList.add('main__content-task-title');
+        const taskTitleElement = document.createElement("h2");
+        taskTitleElement.classList.add("main__content-task-title");
         taskTitleElement.textContent = this.title;
-        const taskDescriptionElement = document.createElement('p');
-        taskDescriptionElement.classList.add('main__content-task-title');
+        const taskDescriptionElement = document.createElement("p");
+        taskDescriptionElement.classList.add("main__content-task-description");
         taskDescriptionElement.textContent = this.description;
-        const taskRemoveIcon = document.createElement('img');
-        taskRemoveIcon.classList.add('main__content-task-remove');
+        const taskPriorityElement = document.createElement("p");
+        taskPriorityElement.classList.add("main__content-task-priority");
+        taskPriorityElement.textContent = this.priority;
+        const taskDueDateElement = document.createElement("p");
+        taskDueDateElement.classList.add("main__content-task-due-date");
+        taskDueDateElement.textContent = this.dueDate;
+        const taskEditIcon = document.createElement("img");
+        taskEditIcon.classList.add("main__content-task-edit");
+        taskEditIcon.src = Edit;
+        taskEditIcon.title = "Edit Task Icon";
+        const taskRemoveIcon = document.createElement("img");
+        taskRemoveIcon.classList.add("main__content-task-remove");
         taskRemoveIcon.src = Remove;
-        taskRemoveIcon.textContent = 'Remove';
+        taskRemoveIcon.title = "Remove Task Icon";
         taskElement.appendChild(taskTitleElement);
         taskElement.appendChild(taskDescriptionElement);
+        taskElement.appendChild(taskPriorityElement);
+        taskElement.appendChild(taskDueDateElement);
+        taskElement.appendChild(taskEditIcon);
         taskElement.appendChild(taskRemoveIcon);
         return taskElement;
-
     }
-
 }
