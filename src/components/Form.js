6f22242bafca2416e31ec8282f44c44fd9f85802
p1zzakat <rbanine@gmail.com>
2025-10-04 13:35:45 +0100

    Update Form UI, Fix Task Edit Form and other bugs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,42 +1,74 @@
+import { capitalizeEachWord } from "../utils/capitalizeEachWord";
+
 import Close from "../public/remove.svg";
 
 export class Form {
     constructor(formClassPrefix) {
         this.formClassPrefix = formClassPrefix;
+        this.formTitle = capitalizeEachWord(formClassPrefix.replaceAll("-", " "));
         this.formElement = this.constructElement();
         this.displayForm();
     }
 
     constructElement() {
+        // # Form
         const formElement = document.createElement("div");
         formElement.classList.add(`${this.formClassPrefix}`, "form");
+
+        // ## Header
+
+        const formHeader = document.createElement("div");
+        formHeader.classList.add("form-header");
+
+        // ## Header Title
+
+        const formTitle = document.createElement("h2");
+
+        formTitle.textContent = this.formTitle;
+        formHeader.appendChild(formTitle);
+
+        // ## Header Exit
 
         const formExit = document.createElement("img");
         formExit.classList.add(`${this.formClassPrefix}__close`);
         formExit.src = Close;
-        formElement.appendChild(formExit);
+        formHeader.appendChild(formExit);
+
+        // ## Body 
+
+        const formBody = document.createElement("div");
+        formBody.classList.add("form-body");
+        
+        // ## Body Input Title
 
         const formInputElementTitle = document.createElement("input");
         formInputElementTitle.classList.add(`${this.formClassPrefix}__input-title`);
         formInputElementTitle.placeholder = "Title";
-        formElement.appendChild(formInputElementTitle);
+        formBody.appendChild(formInputElementTitle);
+
+        // ## Body Input Description
 
         const formInputElementDescription = document.createElement("input");
         formInputElementDescription.classList.add(`${this.formClassPrefix}__input-description`);
         formInputElementDescription.placeholder = "Description";
-        formElement.appendChild(formInputElementDescription);
+        formBody.appendChild(formInputElementDescription);
+
+        // ## Body Input Fields
 
         const fields = this.getFields();
-        fields.forEach(field => formElement.appendChild(field));
+        fields.forEach(field => formBody.appendChild(field));
         
+        // ## Body Submit
 
         const formSubmit = document.createElement("button");
         formSubmit.classList.add(`${this.formClassPrefix}__button-submit`);
         formSubmit.textContent = "Submit";
-        formElement.appendChild(formSubmit);
+        formBody.appendChild(formSubmit);
 
         this.formInputElementTitle = formInputElementTitle;
         this.formInputElementDescription = formInputElementDescription;
+        formElement.appendChild(formHeader);
+        formElement.appendChild(formBody);
         return formElement;
     }
 
--- src/components/TaskEditForm.js
+++ src/components/TaskEditForm.js
@@ -14,28 +14,47 @@ export class TaskEditForm extends Form {
     getFields() {
 
         const priorityField = document.createElement("div");
+        priorityField.classList.add("priority-field");
+        priorityField.classList.add("field");
+
         const priorityFieldLabel = document.createElement("label");
-        priorityFieldLabel.htmlFor = "priority"
+        priorityFieldLabel.textContent = "Priority:";
+        priorityFieldLabel.htmlFor = "priority";
+        priorityField.appendChild(priorityFieldLabel);
+
         const priorityFieldSelect = document.createElement("select");
+
         const priorityHigh = document.createElement("option");
         priorityHigh.value = "High";
         priorityHigh.textContent = "High";
+        priorityFieldSelect.appendChild(priorityHigh);
+
         const priorityNormal = document.createElement("option");
         priorityNormal.value = "Normal";
         priorityNormal.textContent = "Normal";
+        priorityFieldSelect.appendChild(priorityNormal);
+
         const priorityLow = document.createElement("option");
         priorityLow.value = "Low";
         priorityLow.textContent = "Low";
-
-        priorityFieldSelect.appendChild(priorityHigh);
-        priorityFieldSelect.appendChild(priorityNormal);
         priorityFieldSelect.appendChild(priorityLow);
+
         priorityField.appendChild(priorityFieldLabel);
         priorityField.appendChild(priorityFieldSelect);
 
+        const dueDateField = document.createElement("div");
+        dueDateField.classList.add("due-date-field");
+        dueDateField.classList.add("field");
 
-        const dueDateField = document.createElement("input");
-        dueDateField.type = "datetime-local";
+        const dueDateFieldLabel = document.createElement("label");
+        dueDateFieldLabel.classList.add("due-date-field-label");
+        dueDateFieldLabel.textContent = "Date:";
+        dueDateField.appendChild(dueDateFieldLabel);
+
+        const dueDateFieldInput = document.createElement("input");
+        dueDateFieldInput.classList.add("due-date-field-input");
+        dueDateFieldInput.type = "date";
+        dueDateField.appendChild(dueDateFieldInput);
 
 
         return [
